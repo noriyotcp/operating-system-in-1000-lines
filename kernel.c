d8f7eef80da33a39572c2dcadaf61023b8a4d846
noriyotcp <noriyo.akita@gmail.com>
2023-08-12 00:20:51 +0900

    Define typedef
    Oops, I've been missing.

--- kernel.c
+++ kernel.c
@@ -1,15 +1,18 @@
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
+
 extern char __bss[], __bss_end[], __stack_top[];
 
-void *memset(void *buf, char c, unsigned int n)
-{
-    unsigned char *p = (unsigned char *) buf;
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
     while (n--)
         *p++ = c;
     return buf;
 }
 
 void kernel_main(void) {
-    memset(__bss, 0, (unsigned int) __bss_end - (unsigned int) __bss);
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
     for(;;);
 }
