058d8b9899724c70baf3590455ec8d8ed16b9ff6
noriyotcp <noriyo.akita@gmail.com>
2023-08-12 00:21:47 +0900

    Update run.sh
    In order to run the kernel

--- run.sh
+++ run.sh
@@ -4,7 +4,17 @@ set -xue
 
 # QEMU のファイルパス
 QEMU=qemu-system-riscv32
+# ls $(brew --prefix)/opt/llvm/bin/clang
+# CC=/usr/local/opt/llvm/bin/clang
+# on Ubuntu
+CC=/usr/bin/clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
 
 # QEMU を起動
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
-
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
