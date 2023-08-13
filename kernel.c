9f8f1c13bbed3a7e2703d4b263a3cfdbd2298107
noriyotcp <noriyo.akita@gmail.com>
2023-08-13 21:52:56 +0900

    Implement context switch

--- kernel.c
+++ kernel.c
@@ -4,6 +4,8 @@
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
 
+struct process procs[PROCS_MAX];
+
 paddr_t alloc_pages(uint32_t n) {
     static paddr_t next_paddr = (paddr_t) __free_ram;
     paddr_t paddr = next_paddr;
@@ -116,6 +118,80 @@ void kernel_entry(void) {
         "sret\n"
     );
 }
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp) {
+    __asm__ __volatile__(
+        "addi sp, sp, -13 * 4\n"
+        "sw ra,  0 * 4(sp)\n"
+        "sw s0,  1 * 4(sp)\n"
+        "sw s1,  2 * 4(sp)\n"
+        "sw s2,  3 * 4(sp)\n"
+        "sw s3,  4 * 4(sp)\n"
+        "sw s4,  5 * 4(sp)\n"
+        "sw s5,  6 * 4(sp)\n"
+        "sw s6,  7 * 4(sp)\n"
+        "sw s7,  8 * 4(sp)\n"
+        "sw s8,  9 * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+        "sw sp, (a0)\n"
+        "lw sp, (a1)\n"
+        "lw ra,  0 * 4(sp)\n"
+        "lw s0,  1 * 4(sp)\n"
+        "lw s1,  2 * 4(sp)\n"
+        "lw s2,  3 * 4(sp)\n"
+        "lw s3,  4 * 4(sp)\n"
+        "lw s4,  5 * 4(sp)\n"
+        "lw s5,  6 * 4(sp)\n"
+        "lw s6,  7 * 4(sp)\n"
+        "lw s7,  8 * 4(sp)\n"
+        "lw s8,  9 * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"
+        "ret\n"
+    );
+}
+
+struct process *create_process(uint32_t pc) {
+    // 空いているプロセス管理構造体を探す
+    struct process *proc = &procs[0];
+    int i;
+    for (i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    // switch_context() で復帰できるように、スタックに呼び出し先保存レジスタを積む
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    // 各フィールドを初期化
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+};
 
 void handle_trap(struct trap_frame *f) {
     uint32_t scause = READ_CSR(scause);
