4e046aea2149f6caf9b86073a20c871ff14acf2f
noriyotcp <noriyo.akita@gmail.com>
2023-08-15 03:14:33 +0900

    Add condition for macOS or others

--- run.sh
+++ run.sh
@@ -4,13 +4,20 @@ set -xue
 
 # QEMU のファイルパス
 QEMU=qemu-system-riscv32
-# ls $(brew --prefix)/opt/llvm/bin/clang
-# CC=/usr/local/opt/llvm/bin/clang
-# on Ubuntu
-CC=$(which clang)
-# ls $(brew --prefix)/opt/llvm/bin/llvm-objcopy
-# OBJCOPY=/usr/local/opt/llvm/bin/llvm-objcopy
-OBJCOPY=$(which llvm-objcopy)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # ls $(brew --prefix)/opt/llvm/bin/clang
+  CC=/usr/local/opt/llvm/bin/clang
+else
+  # on Ubuntu
+  CC=$(which clang)
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # ls $(brew --prefix)/opt/llvm/bin/llvm-objcopy
+  OBJCOPY=/usr/local/opt/llvm/bin/llvm-objcopy
+else
+  OBJCOPY=$(which llvm-objcopy)
+fi
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
