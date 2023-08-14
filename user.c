5dccb1ce9b8c8560e32a77ea0027afbd794e37b7
noriyotcp <noriyo.akita@gmail.com>
2023-08-15 01:35:04 +0900

    Implement sys_call() and putchar()

--- user.c
+++ user.c
@@ -2,12 +2,26 @@
 
 extern char __stack_top[];
 
-__attribute__((noreturn)) void exit(void) {
-    for (;;);
+int syscall(int sysno, int arg0, int arg1, int arg2) {
+    register int a0 __asm__("a0") = arg0;
+    register int a1 __asm__("a1") = arg1;
+    register int a2 __asm__("a2") = arg2;
+    register int a3 __asm__("a3") = sysno;
+
+    __asm__ __volatile__("ecall"
+                         : "=r"(a0)
+                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3)
+                         : "memory");
+
+    return a0;
 }
 
 void putchar(char ch) {
-    // 後で実装する
+    syscall(SYS_PUTCHAR, ch, 0, 0);
+}
+
+__attribute__((noreturn)) void exit(void) {
+    for (;;);
 }
 
 __attribute__((section(".text.start")))
