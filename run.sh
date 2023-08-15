18b913b97851cd260720e28d48ac2efe0cd0ac0a
noriyotcp <noriyo.akita@gmail.com>
2023-08-15 23:20:01 +0900

    Add QEMU options for virtio
    and create lorem.txt

--- run.sh
+++ run.sh
@@ -33,4 +33,6 @@ $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
 # QEMU を起動
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
     -kernel kernel.elf
