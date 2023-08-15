642766dd8e789494006572aa07672b3a0e657aa5
noriyotcp <noriyo.akita@gmail.com>
2023-08-16 00:40:08 +0900

    Send IO request

--- kernel.c
+++ kernel.c
@@ -67,6 +67,21 @@ void virtio_reg_fetch_and_or32(unsigned offset, uint32_t value) {
     virtio_reg_write32(offset, virtio_reg_read32(offset) | value);
 }
 
+// デバイスに新しいリクエストがあることを通知する。desc_index は、新しいリクエストの
+// 先頭ディスクリプタのインデックス
+void virtq_kick(struct virtio_virtq *vq, int desc_index) {
+    vq->avail.ring[vq->avail.index % VIRTQ_ENTRY_NUM] = desc_index;
+    vq->avail.index++;
+    __sync_synchronize();
+    virtio_reg_write32(VIRTIO_REG_QUEUE_NOTIFY, vq->queue_index);
+    vq->last_used_index++;
+}
+
+// デバイスが処理中のリクエストがあるかどうかを返す
+bool virtq_is_busy(struct virtio_virtq *vq) {
+    return vq->last_used_index != *vq->used_index;
+}
+
 struct virtio_virtq *virtq_init(unsigned index) {
     paddr_t virtq_paddr = alloc_pages(align_up(sizeof(struct virtio_virtq), PAGE_SIZE) / PAGE_SIZE);
     struct virtio_virtq *vq = (struct virtio_virtq *) virtq_paddr;
@@ -111,6 +126,54 @@ void virtio_blk_init(void) {
     // デバイスへの処理要求を格納する領域を確保する
     blk_req_paddr = alloc_pages(align_up(sizeof(*blk_req), PAGE_SIZE) / PAGE_SIZE);
     blk_req = (struct virtio_blk_req *) blk_req_paddr;
+}
+
+// read/write for virtio-blk device
+void read_write_disk(void *buf, unsigned sector, int is_write) {
+    if (sector >= blk_capacity / SECTOR_SIZE) {
+        printf("virtio: tried to read/write sector=%d, but capacity is %d\n", sector, blk_capacity / SECTOR_SIZE);
+        return;
+    }
+
+    // virtio-blk の使用に従って、リクエストを構築する
+    blk_req->sector = sector;
+    blk_req->type = is_write ? VIRTIO_BLK_T_OUT : VIRTIO_BLK_T_IN;
+
+    if (is_write)
+        memcpy(blk_req->data, buf, SECTOR_SIZE);
+
+    // virtqueue のディスクリプタを構築する（3つのディスクリプタを使う）
+    struct virtio_virtq *vq = blk_request_vq;
+    vq->descs[0].addr = blk_req_paddr;
+    vq->descs[0].len = sizeof(uint32_t) * 2 + sizeof(uint64_t);
+    vq->descs[0].flags = VIRTQ_DESC_F_NEXT;
+    vq->descs[0].next = 1;
+
+    vq->descs[1].addr = blk_req_paddr + offsetof(struct virtio_blk_req, data);
+    vq->descs[1].len = SECTOR_SIZE;
+    vq->descs[1].flags = VIRTQ_DESC_F_NEXT | (is_write ? 0 : VIRTQ_DESC_F_WRITE);
+    vq->descs[1].next = 2;
+
+    vq->descs[2].addr = blk_req_paddr + offsetof(struct virtio_blk_req, status);
+    vq->descs[2].len = sizeof(uint8_t);
+    vq->descs[2].flags = VIRTQ_DESC_F_WRITE;
+
+    // デバイスに新しいリクエストがあることを通知する
+    virtq_kick(vq, 0);
+
+    // デバイス側の処理が終わるのを待つ
+    while (virtq_is_busy(vq))
+        ;
+
+    // virtio-blk: 0 ではない値が帰ってきたらエラー
+    if (blk_req->status != 0) {
+        printf("virtio: warn: failed to read/write sector=%d status=%d\n", sector, blk_req->status);
+        return;
+    }
+
+    // 読み込み処理の場合は、バッファにデータをコピーする
+    if (!is_write)
+        memcpy(buf, blk_req->data, SECTOR_SIZE);
 }
 
 void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
