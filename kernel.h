9269883b69cd334f81b87111f2530a9834207626
noriyotcp <noriyo.akita@gmail.com>
2023-08-17 21:40:15 +0900

    Format kernel.h

--- kernel.h
+++ kernel.h
@@ -8,177 +8,179 @@
 #define SATP_SV32 (1u << 31)
 #define SSTATUS_SPIE (1 << 5)
 #define SCAUSE_ECALL 8
-#define PAGE_V    (1 << 0) // 有効化ビット
-#define PAGE_R    (1 << 1) // 読み込み可能
-#define PAGE_W    (1 << 2) // 書き込み可能
-#define PAGE_X    (1 << 3) // 実行可能
-#define PAGE_U    (1 << 4) // ユーザーモードでアクセス可能
+#define PAGE_V (1 << 0) // 有効化ビット
+#define PAGE_R (1 << 1) // 読み込み可能
+#define PAGE_W (1 << 2) // 書き込み可能
+#define PAGE_X (1 << 3) // 実行可能
+#define PAGE_U (1 << 4) // ユーザーモードでアクセス可能
 #define USER_BASE 0x1000000
 #define FILES_MAX 2
 #define DISK_MAX_SIZE align_up(sizeof(struct file) * FILES_MAX, SECTOR_SIZE)
 
-#define SECTOR_SIZE       512
-#define VIRTQ_ENTRY_NUM   16
+#define SECTOR_SIZE 512
+#define VIRTQ_ENTRY_NUM 16
 #define VIRTIO_DEVICE_BLK 2
-#define VIRTIO_BLK_PADDR  0x10001000
-#define VIRTIO_REG_MAGIC         0x00
-#define VIRTIO_REG_VERSION       0x04
-#define VIRTIO_REG_DEVICE_ID     0x08
-#define VIRTIO_REG_QUEUE_SEL     0x30
+#define VIRTIO_BLK_PADDR 0x10001000
+#define VIRTIO_REG_MAGIC 0x00
+#define VIRTIO_REG_VERSION 0x04
+#define VIRTIO_REG_DEVICE_ID 0x08
+#define VIRTIO_REG_QUEUE_SEL 0x30
 #define VIRTIO_REG_QUEUE_NUM_MAX 0x34
-#define VIRTIO_REG_QUEUE_NUM     0x38
-#define VIRTIO_REG_QUEUE_ALIGN   0x3c
-#define VIRTIO_REG_QUEUE_PFN     0x40
-#define VIRTIO_REG_QUEUE_READY   0x44
-#define VIRTIO_REG_QUEUE_NOTIFY  0x50
+#define VIRTIO_REG_QUEUE_NUM 0x38
+#define VIRTIO_REG_QUEUE_ALIGN 0x3c
+#define VIRTIO_REG_QUEUE_PFN 0x40
+#define VIRTIO_REG_QUEUE_READY 0x44
+#define VIRTIO_REG_QUEUE_NOTIFY 0x50
 #define VIRTIO_REG_DEVICE_STATUS 0x70
 #define VIRTIO_REG_DEVICE_CONFIG 0x100
-#define VIRTIO_STATUS_ACK       1
-#define VIRTIO_STATUS_DRIVER    2
+#define VIRTIO_STATUS_ACK 1
+#define VIRTIO_STATUS_DRIVER 2
 #define VIRTIO_STATUS_DRIVER_OK 4
-#define VIRTIO_STATUS_FEAT_OK   8
-#define VIRTQ_DESC_F_NEXT          1
-#define VIRTQ_DESC_F_WRITE         2
+#define VIRTIO_STATUS_FEAT_OK 8
+#define VIRTQ_DESC_F_NEXT 1
+#define VIRTQ_DESC_F_WRITE 2
 #define VIRTQ_AVAIL_F_NO_INTERRUPT 1
-#define VIRTIO_BLK_T_IN  0
+#define VIRTIO_BLK_T_IN 0
 #define VIRTIO_BLK_T_OUT 1
 
 struct process {
-  int pid;              // process ID
-  int state;            // プロセスの状態
-  vaddr_t sp;           // コンテキストスイッチ時のスタックポインタ
-  uint32_t *page_table;
-  uint8_t stack[8192];  // カーネルスタック
+    int pid;    // process ID
+    int state;  // プロセスの状態
+    vaddr_t sp; // コンテキストスイッチ時のスタックポインタ
+    uint32_t *page_table;
+    uint8_t stack[8192]; // カーネルスタック
 };
 
 struct sbiret {
-  long error;
-  long value;
+    long error;
+    long value;
 };
 
 struct trap_frame {
-  uint32_t ra;
-  uint32_t gp;
-  uint32_t tp;
-  uint32_t t0;
-  uint32_t t1;
-  uint32_t t2;
-  uint32_t t3;
-  uint32_t t4;
-  uint32_t t5;
-  uint32_t t6;
-  uint32_t a0;
-  uint32_t a1;
-  uint32_t a2;
-  uint32_t a3;
-  uint32_t a4;
-  uint32_t a5;
-  uint32_t a6;
-  uint32_t a7;
-  uint32_t s0;
-  uint32_t s1;
-  uint32_t s2;
-  uint32_t s3;
-  uint32_t s4;
-  uint32_t s5;
-  uint32_t s6;
-  uint32_t s7;
-  uint32_t s8;
-  uint32_t s9;
-  uint32_t s10;
-  uint32_t s11;
-  uint32_t sp;
+    uint32_t ra;
+    uint32_t gp;
+    uint32_t tp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t sp;
 } __attribute__((packed));
 
 struct virtq_desc {
-  uint64_t addr;
-  uint32_t len;
-  uint16_t flags;
-  uint16_t next;
+    uint64_t addr;
+    uint32_t len;
+    uint16_t flags;
+    uint16_t next;
 } __attribute__((packed));
 
 struct virtq_avail {
-  uint16_t flags;
-  uint16_t index;
-  uint16_t ring[VIRTQ_ENTRY_NUM];
+    uint16_t flags;
+    uint16_t index;
+    uint16_t ring[VIRTQ_ENTRY_NUM];
 } __attribute__((packed));
 
 struct virtq_used_elem {
-  uint32_t id;
-  uint32_t len;
+    uint32_t id;
+    uint32_t len;
 } __attribute__((packed));
 
 struct virtq_used {
-  uint16_t flags;
-  uint16_t index;
-  struct virtq_used_elem ring[VIRTQ_ENTRY_NUM];
+    uint16_t flags;
+    uint16_t index;
+    struct virtq_used_elem ring[VIRTQ_ENTRY_NUM];
 } __attribute__((packed));
 
 struct virtio_virtq {
-  struct virtq_desc descs[VIRTQ_ENTRY_NUM];
-  struct virtq_avail avail;
-  struct virtq_used used __attribute__((aligned(PAGE_SIZE)));
-  int queue_index;
-  volatile uint16_t *used_index;
-  uint16_t last_used_index;
+    struct virtq_desc descs[VIRTQ_ENTRY_NUM];
+    struct virtq_avail avail;
+    struct virtq_used used __attribute__((aligned(PAGE_SIZE)));
+    int queue_index;
+    volatile uint16_t *used_index;
+    uint16_t last_used_index;
 } __attribute__((packed));
 
 struct virtio_blk_req {
-  // 1つ目のディスクリプタ: デバイスからは読み込み専用
-  uint32_t type;
-  uint32_t reserved;
-  uint64_t sector;
+    // 1つ目のディスクリプタ: デバイスからは読み込み専用
+    uint32_t type;
+    uint32_t reserved;
+    uint64_t sector;
 
-  // 2つ目のディスクリプタ: 読み込み処理の場合は、デバイスから書き込み可 (VIRTQ_DESC_F_WRITE)
-  uint8_t data[SECTOR_SIZE];
+    // 2つ目のディスクリプタ: 読み込み処理の場合は、デバイスから書き込み可
+    // (VIRTQ_DESC_F_WRITE)
+    uint8_t data[SECTOR_SIZE];
 
-  // 3つ目のディスクリプタ: デバイスから書き込み可 (VIRTQ_DESC_F_WRITE)
-  uint8_t status;
+    // 3つ目のディスクリプタ: デバイスから書き込み可 (VIRTQ_DESC_F_WRITE)
+    uint8_t status;
 } __attribute__((packed));
 
 struct tar_header {
-  char name[100];
-  char mode[8];
-  char uid[8];
-  char gid[8];
-  char size[12];
-  char mtime[12];
-  char checksum[8];
-  char type;
-  char linkname[100];
-  char magic[6];
-  char version[2];
-  char uname[32];
-  char gname[32];
-  char devmajor[8];
-  char devminor[8];
-  char prefix[155];
-  char padding[12];
-  char data[]; // ヘッダに続くデータ領域を指す配列 (フレキシブル配列メンバ)
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char checksum[8];
+    char type;
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char padding[12];
+    char data[]; // ヘッダに続くデータ領域を指す配列 (フレキシブル配列メンバ)
 } __attribute__((packed));
 
 struct file {
-  bool in_use;      // このファイルエントリが使われているか
-  char name[100];   // ファイル名
-  char data[1024];  // ファイルの内容
-  size_t size;      // ファイルサイズ
+    bool in_use;     // このファイルエントリが使われているか
+    char name[100];  // ファイル名
+    char data[1024]; // ファイルの内容
+    size_t size;     // ファイルサイズ
 };
 
 #define READ_CSR(reg)                                                          \
-  ({                                                                           \
-      unsigned long __tmp;                                                     \
-      __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                    \
-      __tmp;                                                                   \
-  })
+    ({                                                                         \
+        unsigned long __tmp;                                                   \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
+        __tmp;                                                                 \
+    })
 
 #define WRITE_CSR(reg, value)                                                  \
-  do {                                                                         \
-      uint32_t __tmp = (value);                                                \
-      __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                   \
-  } while (0)
+    do {                                                                       \
+        uint32_t __tmp = (value);                                              \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
+    } while (0)
 
-#define PANIC(fmt, ...)                                                   \
-  do {                                                                    \
-    printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
-    while (1) {}                                                          \
-  } while (0)
+#define PANIC(fmt, ...)                                                        \
+    do {                                                                       \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {                                                            \
+        }                                                                      \
+    } while (0)
